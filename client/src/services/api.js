62aead9ab78b5e0b241cbad7ba0bb1c7ef032fae
sabakk <sabakk@ukr.net>
2019-08-24 17:25:33 +0300

    add api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,92 +1,94 @@
-import axios from 'axios';
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:5000'
-axios.interceptors.response.use(res=> res.data, err=> Promise.reject(err.response))
+// axios.defaults.baseURL = 'http://localhost:5000'
+axios.interceptors.response.use(
+  res => res.data,
+  err => Promise.reject(err.response)
+);
 
 export const setAuthToken = token => {
-    if (token) {
-      axios.defaults.headers.common['x-auth-token'] = token;
-    } else {
-      delete axios.defaults.headers.common['x-auth-token'];
-    }
+  if (token) {
+    axios.defaults.headers.common["x-auth-token"] = token;
+  } else {
+    delete axios.defaults.headers.common["x-auth-token"];
   }
+};
 // const getAuthHeader = () => ({Authorization: `Bearer ${store.getState().token}`})
 
 export const ApiService = {
-
   // auth
 
-    login(item) {
-        return axios.post('/api/auth', item)  
-    },
+  login(item) {
+    return axios.post("/api/auth", item);
+  },
 
-    signup(item) {
-        return axios.post('/api/users', item)  
-    },
+  signup(item) {
+    return axios.post("/api/users", item);
+  },
 
-    auth() {
-        return axios.get('/api/auth')
-    },
+  auth() {
+    return axios.get("/api/auth");
+  },
 
-// Profile
+  // Profile
 
-    getCurrentProfile() { 
-       return axios.get('/api/profile/me')
-    },
+  getCurrentProfile() {
+    return axios.get("/api/profile/me");
+  },
 
-    getProfiles() {
-      return axios.get('/api/profile')
-    },
+  getProfiles() {
+    return axios.get("/api/profile");
+  },
 
-    getProfileById(userId) {
-      return axios.get(`/api/profile/user/${userId}`)
-    },
-    getGithubRepos(username) {
-      return axios.get(`/api/profile/github/${username}`)
-    },
-    createProfile(formData) {
-      return axios.post('/api/profile', formData)
-    },
-    addExperience(formData){
-      return axios.put('/api/profile/experience', formData)
-    },
-    addEducation(formData){
-      return axios.put('/api/profile/education', formData)
-    },
-    deleteExperience(id){
-      return axios.delete(`/api/profile/experience/${id}`)
-    },
-    deleteEducation(id){
-      return axios.delete(`/api/profile/education/${id}`)
-    },
-    deleteAccount(){
-      return axios.delete('/api/profile')
-    },
+  getProfileById(userId) {
+    return axios.get(`/api/profile/user/${userId}`);
+  },
+  getGithubRepos(username) {
+    return axios.get(`/api/profile/github/${username}`);
+  },
+  createProfile(formData) {
+    return axios.post("/api/profile", formData);
+  },
+  addExperience(formData) {
+    return axios.put("/api/profile/experience", formData);
+  },
+  addEducation(formData) {
+    return axios.put("/api/profile/education", formData);
+  },
+  deleteExperience(id) {
+    return axios.delete(`/api/profile/experience/${id}`);
+  },
+  deleteEducation(id) {
+    return axios.delete(`/api/profile/education/${id}`);
+  },
+  deleteAccount() {
+    return axios.delete("/api/profile");
+  },
 
-    // Vapes
+  // Vapes
 
-    getPosts(){
-      return axios.get('/api/posts')
-    },
-    addPost(formData){
-      return axios.post('/api/posts', formData)
-    },
-    deletePost(id){
-      return axios.delete(`/api/posts/${id}`)
-    },
-    getPost(id){
-      return axios.get(`/api/posts/${id}`)
-    },
-    addComment(postId, formData){
-      return axios.post( `/api/posts/comment/${postId}`, formData)
-    },
-    deleteComment(postId, commentId){
-      return axios.delete(`/api/posts/comment/${postId}/${commentId}`)
-    },
-    addLike(id){
-      return axios.put(`/api/posts/like/${id}`)
-    },
-    removeLike(id){
-      return axios.put(`/api/posts/unlike/${id}`)
-    },
+  getPosts() {
+    return axios.get("/api/posts");
+  },
+  addPost(formData) {
+    return axios.post("/api/posts", formData);
+  },
+  deletePost(id) {
+    return axios.delete(`/api/posts/${id}`);
+  },
+  getPost(id) {
+    return axios.get(`/api/posts/${id}`);
+  },
+  addComment(postId, formData) {
+    return axios.post(`/api/posts/comment/${postId}`, formData);
+  },
+  deleteComment(postId, commentId) {
+    return axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+  },
+  addLike(id) {
+    return axios.put(`/api/posts/like/${id}`);
+  },
+  removeLike(id) {
+    return axios.put(`/api/posts/unlike/${id}`);
   }
+};
